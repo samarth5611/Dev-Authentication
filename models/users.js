47d55d4376ace135b6bb5927820174b3206b1308
devk-232 <56586906+devk-232@users.noreply.github.com>
2021-04-20 16:57:56 +0530

    first comit

---
+++ models/users.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    maxlength: 1024,
+    minlength: 5,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+const User = mongoose.model("User", userSchema);
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin },
+    config.get("jwtPrivateKey")
+  );
+  return token;
+};
+
+
+exports.User = User;
---
+++ routes/user.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const config = require("config");
+const _ = require("lodash");
+const { User } = require("../models/user");
+const auth = require("../middleware/auth");
+const isAdmin = require("../middleware/admin");
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already registered");
+  user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: await bcrypt.hash(req.body.password, 10),
+  });
+  try {
+    await user.save();
+    const token = jwt.sign(
+      { _id: user._id, isAdmin: user.isAdmin },
+      config.get("jwtPrivateKey")
+    );
+    res
+      .header("x-auth-token", token)
+      .header("access-control-expose-headers", "x-auth-token")
+      .send(_.pick(user, ["_id", "name", "email"]));
+  } catch (err) {
+    console.log("error: ", err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password");
+  if (!user) return res.send("this user doesn't exists in the database!");
+  res.send(user);
+});
+
+
+router.post("/login", async (req, res) => {
+
+  if (req.user) return res.send("User already logged in!");
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email or password");
+
+  const validpassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validpassword) return res.status(400).send("invalid email or password");
+
+  const token = jwt.sign(
+    { _id: user._id, isAdmin: user.isAdmin },
+    config.get("jwtPrivateKey")
+  );
+  res.header("x-auth-token").send(token);
+});
+
+module.exports = router;
